082241bb5dcadaf691c29567cf729019cb2cb0c3
samir-cogoport <samir.dupare@cogoport.com>
2025-06-11 19:32:07 +0530

    update

--- contentScript.js
+++ contentScript.js
@@ -1,48 +1,46 @@
 const buttons = {
-	autoClickSkip: ['button.button-primary.watch-video--skip-content-button[data-uia="player-skip-intro"]'],
-	autoClickSkipRecaps: ['button.button-primary.watch-video--skip-recaps-button', 'button.button-primary.watch-video--skip-content-button[data-uia="player-skip-recap"]'],
-    autoClickNext: ['button.color-primary.hasLabel.hasIcon[data-uia="next-episode-seamless-button"]', 'button.hasLabel.hasIcon[data-uia="next-episode-seamless-button-draining"]'],
+	autoClickSkip       : ['button.button-primary.watch-video--skip-content-button[data-uia="player-skip-intro"]'],
+	autoClickSkipRecaps : ['button.button-primary.watch-video--skip-recaps-button', 'button.button-primary.watch-video--skip-content-button[data-uia="player-skip-recap"]'],
+	autoClickNext       : ['button.color-primary.hasLabel.hasIcon[data-uia="next-episode-seamless-button"]', 'button.hasLabel.hasIcon[data-uia="next-episode-seamless-button-draining"]'],
 };
+
+function isVisible(elem) {
+	if (!elem) return false;
+	const style = window.getComputedStyle(elem);
+	return style.display !== 'none' && style.visibility !== 'hidden';
+}
 
 function detectAndClick() {
 	try {
-		chrome.storage.local.get(["autoClickNext", "autoClickSkip", "autoClickSkipRecaps"], (data) => {
+		chrome.storage.local.get(['autoClickNext', 'autoClickSkip', 'autoClickSkipRecaps'], (data) => {
 			for (const [key, selectors] of Object.entries(buttons)) {
-				if (!data[key]) {
-					continue
-				}; 
-				const button = selectors.map(selector => document.querySelector(selector)).find(Boolean);
-				console.log(button, data,'buttonHere???')
-				if (button && !button.clicked) {
-					button.clicked = true;
+				if (!data[key]) continue;
+				const button = selectors.map((selector) => document.querySelector(selector)).find(Boolean);
+				if (button && isVisible(button) && !button.hasAttribute('data-nskip-clicked')) {
+					button.setAttribute('data-nskip-clicked', 'true');
 					button.click();
 				}
 			}
 		});
-	}
-	catch(e) {	
-		// console.log(e)
+	} catch (e) {
+		console.log(e);
 	}
 }
 
 const observer = new MutationObserver((mutations) => {
-    for (let mutation of mutations) {
-		if(mutation.type !== "childList" || !mutation.addedNodes.length) {
-			continue
-		}
-        mutation.addedNodes.forEach((node) => {
-            if (node.nodeType === 1) { 
-                detectAndClick();
-            }
-        });
-    }
+	for (const mutation of mutations) {
+	  if (mutation.type !== 'childList' || !mutation.addedNodes.length) continue;
+	  mutation.addedNodes.forEach((node) => {
+			if (node.nodeType === 1) {
+		  detectAndClick();
+			}
+	  });
+	}
 });
-
 
 observer.observe(document.body, {
-    childList: true,
-    subtree: true
+	childList : true,
+	subtree   : true,
 });
-
 
 detectAndClick();
