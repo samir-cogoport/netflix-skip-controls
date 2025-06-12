10e7a8648e0b9379bb346960698c2dd65974e0f0
samir-cogoport <samir.dupare@cogoport.com>
2025-06-12 11:09:12 +0530

    revert

--- background.js
+++ background.js
@@ -1,17 +1,9 @@
-chrome.runtime.onStartup.addListener(() => {
-    chrome.storage.local.set({
-        autoClickNext: true,
-        autoClickSkipRecaps: true,
-        autoClickSkip: true,
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({ autoClickNext: true,autoClickSkipRecaps: true, autoClickSkip: true }, () => {
+        if (chrome.runtime.lastError) {
+            console.error("Error setting storage:", chrome.runtime.lastError);
+        } else {
+            console.log("Default settings saved.");
+        }
     });
 });
-
-chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-    chrome.scripting.executeScript({
-        target: { tabId: details.tabId },
-        files: ['contentScript.js'],
-    });
-});
-
-chrome.alarms.create('keepAlive', { periodInMinutes: 0.1 });
-chrome.alarms.onAlarm.addListener(() => {});
